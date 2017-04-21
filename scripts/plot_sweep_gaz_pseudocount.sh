bf69c94961d86663ef639b54a83d9c52ecd9a95a
Nicholas Andrews <noandrews@gmail.com>
2017-04-21 07:10:45 -0400

    sync

---
+++ scripts/plot_sweep_gaz_pseudocount.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 PATH"
+    exit
+fi
+
+set -e
+
+EPATH=$1
+
+scripts/plot_f1_seaborn.py $EPATH/*/*.dat --output pseudocount_sweep.pdf
+
+# eof
